dde32c412cb4c080cc6cf0f578325e1dfddeb25c
baariq <baariq.fairuuz@dataon.com>
2022-09-27 22:47:23 +0700

    fix bug

--- src/controllers/message.controller.js
+++ src/controllers/message.controller.js
@@ -1,26 +1,36 @@
 const waConfig = require('../../wa.config.json');
 
 const messageController = async (client, message) => {
+    // console.log('messageController');
     const mentions = await message.getMentions();
-    for (let contact of mentions) {
-        if (contact.number === waConfig?.configuration?.botNumber) {
-            if (Array.isArray(waConfig?.mention?.reply)) {
-                const replyLength = waConfig?.mention?.reply.length;
-                const randomIndex = Math.floor(Math.random() * replyLength);
-                const replyText = waConfig?.mention?.reply?.[randomIndex];
-                console.log('reply : ', replyText);
-                message.reply(replyText);
+    if (waConfig?.configuration && waConfig?.configuration?.botNumber && waConfig?.configuration?.masterNumber) {
+        for (let contact of mentions) {
+            if (contact.number === waConfig?.configuration?.botNumber) {
+                if (Array.isArray(waConfig?.mention?.reply)) {
+                    const replyLength = waConfig?.mention?.reply.length;
+                    const randomIndex = Math.floor(Math.random() * replyLength);
+                    const replyText = waConfig?.mention?.reply?.[randomIndex];
+                    console.log('reply : ', replyText);
+                    message.reply(replyText);
+                } else {
+                    const replyText = waConfig?.mention?.reply;
+                    console.log('reply : ', replyText);
+                    message.reply(replyText);
+                }
             }
         }
     }
 
     const messageBody = message.body;
     const lowerCaseMessage = messageBody.toLowerCase();
+    // console.log('lowerCaseMessage : ', lowerCaseMessage);
 
     const replyData = waConfig?.reply;
     if (replyData) {
+        // console.log('replyData : ', replyData);
         for (const i in replyData) {
-            if (replyData?.[i]?.message && replyData?.[i]?.reply && replyData?.[i]?.no) {
+            if (replyData?.[i]?.message && replyData?.[i]?.reply) {
+                // console.log('replyData[i] : ', replyData[i]);
                 let firstCodition = false;
                 let secondCondition = false;
                 if (Array.isArray(replyData?.[i]?.message)) {
@@ -30,6 +40,8 @@ const messageController = async (client, message) => {
                     firstCodition = lowerCaseMessage === replyData?.[i]?.message?.toLowerCase();
                 }
 
+                // console.log('firstCodition : ', firstCodition);
+                // console.log('secondCondition : ', secondCondition);
                 if (firstCodition || secondCondition) {
                     if (Array.isArray(replyData?.[i]?.reply)) {
                         const replyLength = replyData?.[i]?.reply?.length;
