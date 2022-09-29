4a6a32540bcf4bee9280096a29d66ee078b2f587
baariq <baariqazhar@gmail.com>
2022-09-29 12:55:25 +0700

    update

--- src/controllers/ready.controller.js
+++ src/controllers/ready.controller.js
@@ -1,16 +1,76 @@
 const moment = require('moment');
 const chatIdOf = require('../helper/chatIdOf.js');
 const { scheduleControllerPer1, scheduleControllerPer60 } = require('./schedule.controller.js');
+const waConfig = require('../../wa.config.json');
+
+const generateRandom = (min = 0, max = 10) => {
+    // find diff
+    let difference = max - min;
+
+    // generate random number
+    let rand = Math.random();
+
+    // multiply with difference
+    rand = Math.floor(rand * difference);
+
+    // add with min value
+    rand = rand + min;
+
+    return rand;
+};
+
+const randomTime = (time) => {
+    if (!time.includes('-')) {
+        return time;
+    }
+
+    const timeSplit = time.split('-');
+    const timeStart = timeSplit[0];
+    const timeEnd = timeSplit[1];
+
+    const timeStartSplit = timeStart.split(':');
+    const timeStartHour = Number(timeStartSplit[0]);
+    const timeStartMinute = Number(timeStartSplit[1]);
+
+    const timeEndSplit = timeEnd.split(':');
+    const timeEndHour = Number(timeEndSplit[0]);
+    const timeEndMinute = Number(timeEndSplit[1]);
+
+    const hour = generateRandom(timeStartHour, timeEndHour);
+    let minute = generateRandom(0, 59);
+
+    if (hour === timeEndHour && minute > timeEndMinute) {
+        minute = 0;
+    }
+
+    const textHour = hour < 10 ? `0${hour}` : hour;
+    const textMinute = minute < 10 ? `0${minute}` : minute;
+
+    return `${textHour}:${textMinute}`;
+};
 
 const readyController = (client, storage) => {
     const timeNow = moment().format('MMMM Do YYYY, HH:mm:ss');
     console.log('Client is ready!', timeNow);
 
-    const text = `Hi Sir, I am back !!!! on ${timeNow}`;
-    console.log('send :', text);
-    client.sendMessage(chatIdOf('+6287738210702'), text);
+    if (waConfig?.configuration?.masterNumber) {
+        const text = `Hi Sir, I am back !!!! on ${timeNow}`;
+        console.log('send :', text);
+        client.sendMessage(chatIdOf(waConfig?.configuration?.masterNumber), text);
+    }
 
-    setInterval(() => scheduleControllerPer1(client, storage), 1000 * 60);
+    let autoSendData = [...(waConfig?.autoSend || [])];
+    if (waConfig?.autoSend.length > 0) {
+        autoSendData = autoSendData.map((item) => {
+            return {
+                ...item,
+                time: randomTime(item.time),
+            };
+        });
+    }
+    console.log('autoSendData :', autoSendData);
+
+    setInterval(() => scheduleControllerPer1(client, storage, autoSendData), 1000 * 60);
 
     setInterval(() => scheduleControllerPer60(client), 1000 * 60 * 60);
 };
--- src/controllers/schedule.controller.js
+++ src/controllers/schedule.controller.js
@@ -4,13 +4,13 @@ const waConfig = require('../../wa.config.json');
 
 const scheduleControllerPer60 = (client) => {
     const now = moment().format('HH:mm:ss');
-    const text = `I am still alive, Sir on ${now}`;
+    const text = `I am still alive, Sir at ${now}`;
 
     console.log('send : ', text);
     client.sendMessage(chatIdOf('+6287738210702'), text);
 };
 
-const scheduleControllerPer1 = (client, storage) => {
+const scheduleControllerPer1 = (client, storage, autoSendData) => {
     const nowHour = moment().format('HH');
     const nowMinute = moment().format('mm');
     const nowSecond = moment().format('ss');
@@ -27,33 +27,33 @@ const scheduleControllerPer1 = (client, storage) => {
     });
 
     // todo
-    // * O create big plan
+    // * O create big logic
     // * O create logic for random message
     // * create logic for auto send with random hour and minute in range
     // * create based on date
-    const autoSendData = waConfig?.autoSend;
+    // const autoSendData = waConfig?.autoSend;
     if (autoSendData.length > 0) {
         for (const i in autoSendData) {
-            if (!autoSendData?.[i]?.time.includes('-')) {
-                if (autoSendData[i].time === nowHHmm) {
-                    if (Array.isArray(autoSendData?.[i]?.message)) {
-                        const messageLength = autoSendData?.[i]?.message?.length;
-                        const randomIndex = Math.floor(Math.random() * messageLength);
-                        const messageText = autoSendData?.[i]?.message?.[randomIndex];
-                        console.log('send : ', messageText);
-                        client.sendMessage(chatIdOf(autoSendData?.[i]?.no), messageText);
-                    } else {
-                        console.log('send : ', autoSendData?.[i]?.message);
-                        client.sendMessage(chatIdOf(chatIdOf(autoSendData?.[i]?.no)), autoSendData?.[i]?.message);
-                    }
-                    const tempAutoSend = storage.state.autoSend;
-                    tempAutoSend[Number(i)] = storage.state?.autoSend?.[Number(i)] + 1;
-                    storage.setState({
-                        ...storage.state,
-                        autoSend: tempAutoSend,
-                    });
+            // if (!autoSendData?.[i]?.time.includes('-')) {
+            if (autoSendData[i].time === nowHHmm) {
+                if (Array.isArray(autoSendData?.[i]?.message)) {
+                    const messageLength = autoSendData?.[i]?.message?.length;
+                    const randomIndex = Math.floor(Math.random() * messageLength);
+                    const messageText = autoSendData?.[i]?.message?.[randomIndex];
+                    console.log('send : ', messageText);
+                    client.sendMessage(chatIdOf(autoSendData?.[i]?.no), messageText);
+                } else {
+                    console.log('send : ', autoSendData?.[i]?.message);
+                    client.sendMessage(chatIdOf(chatIdOf(autoSendData?.[i]?.no)), autoSendData?.[i]?.message);
                 }
+                const tempAutoSend = storage.state.autoSend;
+                tempAutoSend[Number(i)] = storage.state?.autoSend?.[Number(i)] + 1;
+                storage.setState({
+                    ...storage.state,
+                    autoSend: tempAutoSend,
+                });
             }
+            // }
         }
     }
 };
